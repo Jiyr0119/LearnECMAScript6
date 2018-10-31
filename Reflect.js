056ec582d2277d7cf146e79e49a50bc5c1bde1b5
Jonathon <594402100@qq.com>
2018-10-31 21:04:05 +0800

    be a better man

--- Reflect.js
+++ Reflect.js
@@ -1,4 +1,6 @@
 // reflect get
+import { firstName, lastName, year } from './Module.js';
+
 var myObject = {
     foo: 1,
     bar: 2,
@@ -10,8 +12,17 @@ var myObject = {
 var myReceiverObject = {
     foo: 4,
     bar: 4,
+    set baz(val) {
+        return this.foo = val;
+    }
 };
 // 如果key 相同。 取第三个参数的值。       target,    name,  receiver       
 console.log("Reflect.get", Reflect.get(myObject, 'baz', myReceiverObject)) // 8
 
 console.log("Reflect.get", Reflect.get(myObject, 'baz')) // 3
+
+console.log("Reflect.set", Reflect.set(myReceiverObject, 'baz', 10)) // 3
+
+// let ModuleTest = multiply(11, 5);
+console.log(firstName)
+// console.log('Module.js', ModuleTest)
