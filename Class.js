dbec82d18ad7c172b36c6c884a741756d711e4aa
Jonathon <594402100@qq.com>
2019-01-11 20:12:07 +0800

    add obj2url

--- Class.js
+++ Class.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 class Point {
     constructor(x, y) {
         this.x = x;
@@ -9,3 +10,27 @@ class Point {
 }
 let point = new Point(3, 6);
 console.log('class', point.toString());
+
+let p1 = new Point(3, 7);
+let p2 = new Point(3, 8);
+
+p1.__proto__.printName = function() {
+    return 'Oops'
+};
+
+// console.log(p1.printName())
+
+let p3 = new Point(3, 9)
+    // console.log(p3.printName())
+
+class ColorPoint extends Point {
+    constructor(x, y, color) {
+        super(x, y);
+        this.color = color; // 正确
+    }
+}
+
+let p4 = new ColorPoint(3, 10, 'red');
+console.log('继承', p4);
+console.log('继承', p4.printName());
+console.log('是否继承某一个父类', Object.getPrototypeOf(ColorPoint) === Point)
